136ba3a53fcf9cbf4605fae2da43550f79bdde4e
UJayaP9 <pramuditha.jayasinghe@pearson.com>
2022-01-07 08:26:02 +0530

    Restrict to 10 images and return requested slied number of image array

--- src/controller/carousel.js
+++ src/controller/carousel.js
@@ -2,17 +2,20 @@ const Carousels = require("../models/Carousel");
 
 exports.getSlidesByNumber = (req, res) => {
     const { slides } = req.params;
+    const imageSliderData = [];
 
-    res.status(200).json({ Carousels });
-    // if (productId) {
-    //   Product.findOne({ _id: productId }).exec((error, product) => {
-    //     if (error) return res.status(400).json({ error });
-    //     if (product) {
-    //       //res.status(200).json({ product });
-    //     }
-    //   });
-    // } else {
-    //   return res.status(400).json({ error: "Params required" });
-    // }
+    if (slides) {
+      if (slides <= 10) {
+        //loop images array and get get images from the first index to requesting number
+        for(var i=0; i<Carousels.length; i++){
+          if(i+1 <= slides) {
+            imageSliderData.push(Carousels[i]);
+          }
+        }
+        res.status(200).json({ imageSliderData });
+      } else {
+        return res.status(400).json({ error: "You can retrieve maximum of 10 images" });
+      }
+    }
   };
 
--- src/models/Carousel.js
+++ src/models/Carousel.js
@@ -1,19 +1,54 @@
 const carouselDummyArray = [
     {
-        image: './backend/src/uploads/1.png',
+        image: './backend/src/uploads/1.jpg',
         title: 'image 1',
         subTitle: 'sub title image 1'
     },
     {
-        image: './backend/src/uploads/2.png',
+        image: './backend/src/uploads/2.jpg',
         title: 'image 2',
         subTitle: 'sub title image 2'
     },
     {
-        image: './backend/src/uploads/3.png',
+        image: './backend/src/uploads/3.jpg',
         title: 'image 3',
         subTitle: 'sub title image 3'
     },
+    {
+        image: './backend/src/uploads/4.jpg',
+        title: 'image 4',
+        subTitle: 'sub title image 4'
+    },
+    {
+        image: './backend/src/uploads/5.jpg',
+        title: 'image 5',
+        subTitle: 'sub title image 5'
+    },
+    {
+        image: './backend/src/uploads/6.jpg',
+        title: 'image 6',
+        subTitle: 'sub title image 6'
+    },
+    {
+        image: './backend/src/uploads/7.jpg',
+        title: 'image 7',
+        subTitle: 'sub title image 7'
+    },
+    {
+        image: './backend/src/uploads/8.jpg',
+        title: 'image 8',
+        subTitle: 'sub title image 8'
+    },
+    {
+        image: './backend/src/uploads/9.jpg',
+        title: 'image 9',
+        subTitle: 'sub title image 9'
+    },
+    {
+        image: './backend/src/uploads/10.jpg',
+        title: 'image 10',
+        subTitle: 'sub title image 10'
+    }
 ]
 
 module.exports = carouselDummyArray
